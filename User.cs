873f2410e2744aaa5254403b7fbb741efec763c9
Gus <gusgit@fastmail.org>
2026-01-08 01:19:36 +0100

    Refactor User class

--- User.cs
+++ User.cs
@@ -3,7 +3,18 @@ class User
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
     public string FullName => $"{FirstName} {LastName}";
-    public DateTime DateOfBirth { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+    public int Age
+    {
+        get
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - DateOfBirth.Year - (today.DayOfYear < DateOfBirth.DayOfYear ? 1 : 0);
+            return age;
+        }
+    }
+
     public DateTime LastLogin { get; set; }
     public string Country { get; set; } = "";
     public int DataStored { get; set; } //Syftar till mängden data användaren har lagrat i MB i en tänkt onlinetjänst.
@@ -19,11 +30,18 @@ class User
             {
                 FirstName = Faker.Name.First(),
                 LastName = Faker.Name.Last(),
-                DateOfBirth = Faker.Identification.DateOfBirth(),
+                DateOfBirth = new DateOnly(
+                    Faker.RandomNumber.Next(1960, 2001),
+                    Faker.RandomNumber.Next(1, 12),
+                    Faker.RandomNumber.Next(1, 28)
+                ),
                 Country = Faker.Address.Country(),
                 DataStored = Faker.RandomNumber.Next(100, 9999999),
                 Email = Faker.Internet.Email(),
-                LastLogin = DateTime.Now - TimeSpan.FromDays(Faker.RandomNumber.Next(1, 365))
+                LastLogin = DateTime.Now - TimeSpan.FromDays(Faker.RandomNumber.Next(1, 365),
+                                                             Faker.RandomNumber.Next(0, 24),
+                                                             Faker.RandomNumber.Next(0, 60),
+                                                             Faker.RandomNumber.Next(0, 60))
             });
         }
 
